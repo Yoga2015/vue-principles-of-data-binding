10f042913634178bb77d95eb32f52218d32e718f
Yoga2015 <yoga3333@126.com>
2023-07-25 00:01:39 +0800

    feat : 原生js进行实现Vue双向绑定 对比 Vue 实现双向绑定

--- vue.js
+++ vue.js
@@ -3,9 +3,47 @@ class Vue {
     this.$el = document.querySelector(options.el);
     this.$data = options.data;
     this.$options = options;
+    this.$watchEvent = {};
+    this.proxyData();
+    this.observe();
     this.compile(this.$el);
   }
 
+  // 不想通过this.$data.str, 而想通过 this.str 就可以获取到 数据
+  proxyData() {
+    for (let key in this.$data) {
+      Object.defineProperty(this, key, {
+        get() {
+          return this.$data[key]
+        },
+        set(val) {
+          this.$data[key] = val;
+        }
+      })
+    }
+  }
+
+  // 观察者
+  observe() {
+    for (let key in this.$data) {
+      let value = this.$data[key];
+      let _this = this;
+      Object.defineProperty(this.$data, key, {
+        get() {
+          return value;
+        },
+        set(val) {
+          value = val;
+          // update
+          _this.$watchEvent[key].forEach((item, index) => {
+            item.update();
+          })
+        }
+      })
+    }
+  }
+
+  // 模板编译
   compile(node) {
     node.childNodes.forEach((item, index) => {
       // console.log(item.nodeType);  // 3
@@ -24,6 +62,20 @@ class Vue {
           })
         }
 
+        if (item.hasAttribute('v-model')) {
+          let vmkey = item.getAttribute('v-model');
+          vmkey = vmkey.trim();
+          // console.log(item, vmkey); //<input type="text" v-model="str">  'str'
+
+          item.value = this[vmkey];   // 数据驱动视图
+
+          item.addEventListener('input', () => {
+            this[vmkey] = item.value;
+            console.log(this);
+          })
+
+        }
+
         if (item.childNodes.length > 0) {
           this.compile(item)
         }
@@ -36,10 +88,33 @@ class Vue {
         item.textContent = text.replace(reg, (match, vmKey) => {
           // console.log(match, vmKey);       
           vmKey = vmKey.trim();
+
+          let watcher = new Watch(this, vmKey, item, 'textContent');
+          if (this.$watchEvent[vmKey]) {
+            this.$watchEvent[vmKey].push(watcher);
+          } else {
+            this.$watchEvent[vmKey] = [];
+            this.$watchEvent[vmKey].push(watcher);
+          }
           return this.$data[vmKey];
         })
       }
 
-    })
+    });
+  }
+
+}
+
+
+class Watch {
+  constructor(vm, key, node, attr) {
+    this.vm = vm;
+    this.key = key;
+    this.node = node;
+    this.attr = attr;
+  }
+
+  update() {    // item.textContent = this.$data[vmKey] 
+    this.node[this.attr] = this.vm[this.key];
   }
 }
